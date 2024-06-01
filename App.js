cd703bfa0e142f2276f77fa59fcb0140136b335b
TohidSheikh621 <153414633+TohidSheikh621@users.noreply.github.com>
2024-06-01 16:09:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomNav from './bottom/BottomNav';
+
+
+
+export default function App() {
+  return (
+
+      <NavigationContainer>
+  
+        <BottomNav />
+
+      </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
